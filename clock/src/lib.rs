49474b2819a88a118429da8ac4149fd561dfd560
Praveen Perera <ppraveen25@gmail.com>
2019-11-10 18:05:57 -0500

    Initial clock impl

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,34 @@
-pub struct Clock;
+use std::fmt;
+
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock {
+            hours: hours,
+            minutes: minutes,
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        let hours = match self.hours {
+            24 => 0,
+            _ => self.hours,
+        };
+
+        format!("{:02}:{:02}", hours, self.minutes)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         unimplemented!("Add {} minutes to existing Clock time", minutes);
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02})", self.hours, self.minutes)
     }
 }
