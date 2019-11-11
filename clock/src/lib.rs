1bc2ffb82a1634b1d90f8133673265b121ad7675
Praveen Perera <ppraveen25@gmail.com>
2019-11-11 00:30:31 -0500

    Use display impl instead of to_string

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -13,16 +13,6 @@ impl Clock {
         }
     }
 
-    pub fn to_string(&self) -> String {
-        let hours_from_minutes = self.minutes / 60;
-
-        let hours = (((self.hours + hours_from_minutes) % 24) + 24) % 24;
-
-        let minutes = self.minutes % 60;
-
-        format!("{:02}:{:02}", hours, minutes)
-    }
-
     pub fn add_minutes(&self, minutes: i32) -> Self {
         unimplemented!("Add {} minutes to existing Clock time", minutes);
     }
@@ -30,6 +20,12 @@ impl Clock {
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02})", self.hours, self.minutes)
+        let hours_from_minutes = (self.minutes / 60);
+
+        let hours = (((self.hours + hours_from_minutes) % 24) + 24) % 24;
+
+        let minutes = ((self.minutes + 60) % 60);
+
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
