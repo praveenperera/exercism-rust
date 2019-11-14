bf5123a971eb1b23a0910166fe487c5a60c0b099
Praveen Perera <ppraveen25@gmail.com>
2019-11-14 17:22:48 -0500

    Move logic to Clock new function

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,34 +1,17 @@
 use std::fmt;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Clock {
+    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock {
-            minutes: minutes + (hours * 60),
-        }
-    }
+        let total_minutes = (hours * 60) + minutes;
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock {
-            minutes: self.minutes + minutes,
-        }
-    }
-}
-
-impl PartialEq for Clock {
-    fn eq(&self, other: &Self) -> bool {
-        self.to_string() == other.to_string()
-    }
-}
-
-impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let hours = (self.minutes / 60) % 24;
-        let minutes = self.minutes % 60;
+        let hours = (total_minutes / 60) % 24;
+        let minutes = total_minutes % 60;
 
         // check if minutes are negative and rewind an hour
         let hours = if minutes < 0 { hours - 1 } else { hours };
@@ -37,6 +20,19 @@ impl fmt::Display for Clock {
         let minutes = (minutes + 60) % 60;
         let hours = (hours + 24) % 24;
 
-        write!(f, "{:02}:{:02}", hours, minutes)
+        Clock {
+            hours: hours,
+            minutes: minutes,
+        }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
